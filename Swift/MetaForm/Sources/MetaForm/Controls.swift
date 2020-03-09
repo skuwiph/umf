72a276e4a387ddfecffe856306d239b90213ca7e
Ian Seckington <iseckington@aifs.co.uk>
2020-03-09 16:22:33 +0000

    feat(mfcontrol): add in option base

--- Swift/MetaForm/Sources/MetaForm/Controls.swift
+++ Swift/MetaForm/Sources/MetaForm/Controls.swift
@@ -60,4 +60,80 @@ class MFLabel: MFControl {
         self.text = text
         super.init(parent: parent, controlType: MetaFormControlType.Label, name: name)
     }
+
+    func isValid(form: MetaForm, updateStatus: Bool = true) -> Bool {
+        return true;
+    }
+}
+
+class MFHtmlTextControl: MFControl {
+    var html: string;
+
+    init(parent: MFQuestion, name: String, html: String) {
+        self.html = html
+        super.init(parent: parent, controlType: MetaFormControlType.Html, name: name)
+    }
+
+    func isValid(form: MetaForm, updateStatus: Bool = true) -> Bool {
+        return true;
+    }
+}
+
+class MFTextControl: MFControl {
+    var textType: MetaFormTextType;
+    var maxLength: Int
+    var placeholder: String?
+
+    init(parent: MFQuestion, name: String, textType: MetaFormTextType, maxLength: Int? = 0, placeholder: String?) {
+        self.textType = textType
+        self.maxLength = maxLength
+        self.placeholder = placeholder
+        super.init(parent: parent, controlType: MetaFormControlType.Html, name: name)
+    }
+}
+
+class MFOptionControlBase: MFControl {
+    var options: [MFOptionValue]
+    var optionLayout: ControlLayoutStyle = ControlLayoutStyle.Vertical
+
+    init(parent: MFQuestion, name: String,  options: [MFOptions]) {
+        super.init(a`)
+    }
+}
+
+struct MFOptions {
+    var expandOptions: Bool = true
+    var emptyItem: String?
+    var list: [MFOptionValue]
+    var optionSource: MFOptionSource?
+
+    static OptionFromList(options: [MFOptionValue], emptyItem: String?, expandOptions: Bool = false) -> MFOptionValue {
+        let o = new MFOptions()
+        o.list = options;
+        o.expandOptions = expandOptions
+        o.emptyItem = emptyItem
+
+        return o;
+    }
+
+    static OptionFromUrl(url: String], emptyItem: String?, expandOptions: Bool = false) -> MFOptionValue {
+        let os = new MFOptionSource()
+        os.url = url;
+
+        let o = new MFOptions()
+        o.optionSource = os
+        o.expandOptions = expandOptions
+        o.emptyItem = emptyItem
+
+        return o;
+    }    
+}
+
+struct MFOptionSource {
+    var url: String
+}
+
+struct MFOptionValue {
+    var code: String
+    var description: String
 }
