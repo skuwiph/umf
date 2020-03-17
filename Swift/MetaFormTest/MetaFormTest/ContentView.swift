a58ba6835c0e71ac19194f41f107dffa8b46e2e9
Ian Seckington <iseckington@aifs.co.uk>
2020-03-17 11:08:56 +0000

    init(test-app)

---
+++ Swift/MetaFormTest/MetaFormTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MetaFormTest
+//
+//  Created by Ian Seckington on 17/03/2020.
+//  Copyright © 2020 Ian Seckington. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
